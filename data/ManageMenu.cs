1ab4995c01cc0c163a441e7f015d33ded987bf25
Simen-afk <dunigamber@gmail.com>
2023-12-14 15:49:23 +0300

    pochty

--- data/ManageMenu.cs
+++ data/ManageMenu.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Principal;
 using Unigine;
 
 [Component(PropertyGuid = "5c415a50ae5c6a09b288e3b61a1fc51bb1e060a3")]
@@ -27,6 +28,7 @@ public class ManageMenu : Component
 		void on_close()
 		{
 			windowAbout.RemoveChild(close_icon);
+			gui.RemoveChild(windowAbout);
 			windowAbout.DeleteLater();
 
 		}
@@ -40,10 +42,21 @@ public class ManageMenu : Component
 		WidgetMenuBox Team = new WidgetMenuBox();
 		Team.AddItem("About");
 		//windowAbout.AddChild(testTExt);
-		Team.AddCallback(Gui.CALLBACK_INDEX.CLICKED, () => { if (Team.CurrentItemText == "About") gui.AddChild(windowAbout, Gui.ALIGN_OVERLAP); });
-		close_icon.SetPosition(10, -24);
-		close_icon.AddCallback(Gui.CALLBACK_INDEX.CLICKED, on_close);
-		windowAbout.AddChild(close_icon, Gui.ALIGN_OVERLAP | Gui.ALIGN_RIGHT | Gui.ALIGN_TOP);
+
+		Team.AddCallback(Gui.CALLBACK_INDEX.CLICKED, () =>
+		{
+			if (Team.CurrentItemText == "About" && !Convert.ToBoolean(gui.IsChild(windowAbout)))
+			{
+				close_icon.SetPosition(10, -24);
+				close_icon.AddCallback(Gui.CALLBACK_INDEX.CLICKED, on_close);
+				windowAbout.AddChild(close_icon, Gui.ALIGN_OVERLAP | Gui.ALIGN_RIGHT | Gui.ALIGN_TOP);
+				gui.AddChild(windowAbout, Gui.ALIGN_OVERLAP);
+			}
+			else if (Team.CurrentItemText == "About" && Convert.ToBoolean(gui.IsChild(windowAbout))) { gui.RemoveChild(windowAbout); }
+		});
+		// close_icon.SetPosition(10, -24);
+		// close_icon.AddCallback(Gui.CALLBACK_INDEX.CLICKED, on_close);
+		// windowAbout.AddChild(close_icon, Gui.ALIGN_OVERLAP | Gui.ALIGN_RIGHT | Gui.ALIGN_TOP);
 
 
 		Team.AddItem("Manage Team");
@@ -81,9 +94,4 @@ public class ManageMenu : Component
 		gui.AddChild(topMenu, Gui.ALIGN_OVERLAP);
 	}
 
-	private void Update()
-	{
-		// write here code to be called before updating each render frame
-
-	}
 }
